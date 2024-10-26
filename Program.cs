f3aab3199a28d2df834af0b4176488eeb79b67b3
MuqtadaAhmad <s1023075@uomustansiriyah.edu.iq>
2024-10-26 13:16:07 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+namespace مسودة
+{
+
+   
+    internal class Program
+    {
+         class Stack
+        {
+           public int size;
+            int top;
+            char[] Stackarr;
+
+            public Stack(int ele)
+            {
+                size = ele;
+                top = -1;
+                Stackarr = new char[size];
+            }
+            public bool isEmut()
+            {
+
+                return (top == -1);
+            }
+            public bool isFull()
+            {
+                return (top == size - 1);
+            }
+            public void push (char ele)
+            {
+                Stackarr[++top] = ele;
+            
+            }
+            public char pop()
+            {
+                return Stackarr[top--];
+            }
+            public char Peek()
+            {
+                return Stackarr[top];
+            }
+            public int sizeStack()
+            {
+                return top + 1;
+            
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+       
+
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace مسودة
+{
+     class Stack1
+    {
+        int top;
+        int size;
+        char[] stackarr;
+        public Stack1(int ele)
+        {
+            size = ele;
+             top = -1;
+            stackarr = new char[size];
+        }
+        public void push(char ele)
+        {
+            if (ele == '[' || ele == '{' || ele == '(')
+            {
+                stackarr[++top] = ele;
+            }
+        
+        }
+        public char pop()
+        {
+            return stackarr[top--];
+        }
+        public bool isEmty()
+        {
+            return (top==-1);
+        }
+        public bool isFull()
+        {
+
+            return (top==size-1);
+        }
+        public char peek()
+        {
+            return (stackarr[top]);
+        }
+        public int stackSize()
+        {
+            return top + 1;
+        }
+        public char ip(char x)
+        {
+            switch (x)
+            {
+                case '}':
+                        return '{';
+
+                case ']':
+                    return '[';
+
+                case ')':
+                    return '(';
+            }
+            return '0';
+                
+        }
+
+    }
+}
